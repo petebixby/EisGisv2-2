07d58b4f81e0f74c167a085ceeed3b56cf1897c0
Peter Bixby <petebixby@yahoo.com>
2021-02-23 20:57:16 -0500

    Change login to ask for userid rather than username

--- forms.py
+++ forms.py
@@ -3,16 +3,16 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Userid', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField("Sign in")
-    
-    
+
+
 class GeoCodeAddress(FlaskForm):
     fulladdress = StringField("Full Address", validators=[DataRequired()])
     city = StringField("City", validators=[DataRequired()])
     state = StringField("State", validators=[DataRequired()])
     zipCode = StringField("Zip Code", validators=[DataRequired()])
     submit = SubmitField("Look up")
-    
+
